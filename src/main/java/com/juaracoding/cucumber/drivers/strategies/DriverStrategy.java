156c2fa6637cec0813ad465d6f26a77e6fdbd14a
tessanyyy <tessanykeintjem@gmail.com>
2022-11-05 21:01:44 +0800

    Template

---
+++ src/main/java/com/juaracoding/cucumber/drivers/strategies/DriverStrategy.java
@@ -0,0 +1,9 @@
+package com.juaracoding.cucumber.drivers.strategies;
+
+import org.openqa.selenium.WebDriver;
+
+public interface DriverStrategy {
+
+    WebDriver setStrategy();
+
+}
---
+++ src/main/java/com/juaracoding/cucumber/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.juaracoding.cucumber.utils;
+
+public class Constants {
+
+    public static final int TIMEOUT = 10;
+    public static final String CHROME = "Chrome";
+    public static final String CHROMEHEADLESS = "ChromeHeadless";
+    public static final String FIREFOX = "Firefox";
+    public static final String URL = "https://opensource-demo.orangehrmlive.com";
+
+}
