b79f2b90478edda39a7ea3b5123d9a0d4ea83124
Risna Putri Ramadhani <risnaardn@gmail.com>
2022-11-11 21:22:51 +0700

    test

---
+++ src/main/java/com/juaracoding/cucumber/Main.java
@@ -0,0 +1,7 @@
+package com.juaracoding.cucumber;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/juaracoding/cucumber/pages/Return.java
@@ -0,0 +1,4 @@
+package com.juaracoding.cucumber.pages;
+
+public class Return {
+}
